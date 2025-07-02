d5b42ab2f83204ef5f4e5b0febdf7746b55df9cc
Jonathan Perkins <jonperk318@gmail.com>
2025-07-02 10:20:45 -0400

    Handling connection

--- src/server.c
+++ src/server.c
@@ -1,6 +1,10 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
 
 
 // Constants
@@ -14,64 +18,79 @@ const int MAX_HEADERS = 128;
 
 // Better string struct
 struct BString {
-  char *data;
-  size_t length;
-  size_t capacity;
+    char *data;
+    size_t length;
+    size_t capacity;
 };
 
 struct Bstring *bstring_init(size_t capacity, const char *const s);
 bool bstring_append(struct Bstring *self, const char *const s);
 void bstring_free(struct Bstring *self);
 
+struct Bstring *filename = NULL;
+
 
 // HTTP methods
 enum HTTPMethodType {
-  HTTP_UNKNOWN = 0,
-  HTTP_GET,
-  HTTP_POST,
-  HTTP_PUT,
-  HTTP_PATCH,
-  HTTP_DELETE,
+    HTTP_UNKNOWN,
+    HTTP_GET,
+    HTTP_POST,
+    HTTP_PUT,
+    HTTP_PATCH,
+    HTTP_DELETE,
 };
 
 struct HTTPMethod {
-  const char *const str;
-  enum HTTPMethodType typ;
+    const char *const string;
+    enum HTTPMethodType type;
 };
 
-struct HTTPMethod KNOWN_HTTP_METHODS[] = {
-    {.str = "GET", .typ = HTTP_GET},
-    {.str = "POST", .typ = HTTP_POST},
-    {.str = "PUT", .typ = HTTP_PUT},
-    {.str = "PATCH", .typ = HTTP_PATCH},
-    {.str = "DELETE", .typ = HTTP_DELETE},
+struct HTTPMethod HTTP_METHODS[] = {
+    {.string = "GET", .type = HTTP_GET},
+    {.string = "POST", .type = HTTP_POST},
+    {.string = "PUT", .type = HTTP_PUT},
+    {.string = "PATCH", .type = HTTP_PATCH},
+    {.string = "DELETE", .type = HTTP_DELETE},
 };
 
-size_t KNOWN_HTTP_METHODS_LEN =
-    sizeof(KNOWN_HTTP_METHODS) / sizeof(KNOWN_HTTP_METHODS[0]);
+size_t HTTP_METHODS_LEN =
+    sizeof(HTTP_METHODS) / sizeof(HTTP_METHODS[0]);
 
 struct HTTPHeader {
-  char *key;
-  char *value;
+    char *key;
+    char *value;
 };
 
 struct HTTPRequest {
-  enum HTTPMethodType method;
-  char *path;
-
-  // Headers
-  struct HTTPHeader *headers;
-  size_t headers_len;
-
-  // Request buffer
-  char *_buffer;
-  size_t _buffer_len;
-
-  struct BString *body;
+    enum HTTPMethodType method;
+    char *path;
+    struct HTTPHeader *headers;
+    size_t headers_len;
+    char *_buffer;
+    size_t _buffer_len;
+    struct BString *body;
 };
 
+// Connection
+void handle_connection(int conn_fd);
+int server_fd = -1;
+struct sockaddr_in client_addr;
+socklen_t client_addr_len = sizeof(client_addr);
 
-int main() {
+// MAIN
+int main(int argc, char **argv) {
+
+    // Set directory for public files
+    if (argc > 2) {
+        filename = bstring_init(0, argv[2]);
+        if (argv[2][strlen(argv[2]) - 1] != '/') {
+            bstring_append(filename, "/");
+        }
+    } else {
+        filename = bstring_init(0, "./public/");
+    }
+
+    //printf("Filename: %s\n", filename->data);
     printf("Hello, World!\n");
     return 0;
 }
